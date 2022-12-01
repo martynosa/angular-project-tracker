e42d0ee991563aa5a298e6e53bec94e84a98b352
martiN <78382341+martynosa@users.noreply.github.com>
2022-12-01 16:02:54 +0200

    removed about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { AboutComponent } from './about/about.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
@@ -14,7 +13,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'about', component: AboutComponent },
   { path: '**', component: ErrorPageComponent }, // Wildcard route for a 404 page
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { LandingComponent } from './landing/landing.component';
-import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { NavComponent } from './nav/nav.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
@@ -15,7 +14,6 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     AppComponent,
     CardComponent,
     LandingComponent,
-    AboutComponent,
     ProjectsComponent,
     NavComponent,
     ErrorPageComponent,
